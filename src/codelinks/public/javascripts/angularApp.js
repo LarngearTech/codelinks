99a0981875cc545339e2043304f399e3f7692d3c
Supasate Choochaisri <supasate.c@gmail.com>
2015-02-09 00:47:42 +0700

    Bind addPost button to API.

--- src/codelinks/public/javascripts/angularApp.js
+++ src/codelinks/public/javascripts/angularApp.js
@@ -32,26 +32,25 @@ app.factory('posts', ['$http', function($http) {
             angular.copy(data, o.posts);
         });
     };
+    o.create = function(post) {
+        return $http.post('/posts', post).success(function(data) {
+            o.posts.push(data);
+        });
+    };
     return o;
 }]);
 app.controller('MainCtrl', [
     '$scope',
     'posts',
     function($scope, posts) {
-        $scope.test = 'Hello world!';
         $scope.posts = posts.posts;
         $scope.addPost = function() {
             if (!$scope.title || $scope.title === '') {
                 return;
             }
-            $scope.posts.push({
-                title: $scope.title, 
-                link: $scope.link, 
-                upvotes: 0,
-                comments: [
-                    {author: 'Joe', body: 'Cool posts!', upvotes: 0},
-                    {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
-                ]
+            posts.create({
+                title: $scope.title,
+                link: $scope.link,
             });
             $scope.title = '';
             $scope.link = '';
