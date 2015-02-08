6c9e77ef2d8e023f09e3a4e2471453ad8f775fe7
Supasate Choochaisri <supasate.c@gmail.com>
2015-02-08 12:52:57 +0700

    Install Mongo in both Vagrant and Docker.

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -1,7 +1,8 @@
 # install nodejs git vim
 export DEBIAN_FRONTEND=noninteractive
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10 && echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
 curl -sL https://deb.nodesource.com/setup | bash -
-apt-get update && apt-get install --no-install-recommends -y nodejs git vim
+apt-get update && apt-get install --no-install-recommends -y nodejs git vim mongodb-org
 
 # change global node module directory
 echo prefix = /home/vagrant/.node >> /home/vagrant/.npmrc
