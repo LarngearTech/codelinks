20add12ab20d4ba9b13bea4376e8b12a2b01509e
Supasate Choochaisri <supasate.c@gmail.com>
2015-02-08 11:36:40 +0700

    Bootstap Vagrant with nodejs, git, vim, yo, grunt, bower, generator-meanjs.

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -1,3 +1,13 @@
-# install nodejs
+# install nodejs git vim
+export DEBIAN_FRONTEND=noninteractive
 curl -sL https://deb.nodesource.com/setup | bash -
-apt-get update && apt-get install --no-install-recommends -y nodejs
+apt-get update && apt-get install --no-install-recommends -y nodejs git vim
+
+# change global node module directory
+echo prefix = /home/vagrant/.node >> /home/vagrant/.npmrc
+echo "export PATH=\$PATH:/home/vagrant/.node/bin" >> /home/vagrant/.bashrc
+echo "export NODE_PATH=\$NODE_PATH:/home/vagrant/.node/lib/node_modules" >> /home/vagrant/.bashrc
+source /home/vagrant/.bashrc
+
+# install yo grunt bower generator-meanjs as non-root user
+su -c "npm install -g yo grunt-cli bower generator-meanjs" vagrant
